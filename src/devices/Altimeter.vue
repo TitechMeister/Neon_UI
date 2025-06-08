5e0120d135b8453d9fb2c96b548f8ea429056da8
Alter <tuuchan0420@gmail.com>
2025-06-08 14:55:56 +0900

    コード分割した

---
+++ src/devices/Altimeter.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { ref,watch, onMounted } from 'vue'
+
+interface AltimeterData {
+  altitude: number;
+  timestamp: string;
+}
+
+const altitude = ref<AltimeterData>();
+const isUpdateConstant = ref<boolean>(false);
+
+// component logic
+async function fetchData() {
+  // Simulate fetching data
+  const url = './api/altimeter';
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`レスポンスステータス: ${response.status}`);
+    }
+    console.log('Response received:', response);
+    altitude.value = await response.json();
+  } catch (error: any) {
+    console.error(error.message);
+  }
+}
+
+const fetchDataInInterval = () => {
+  fetchData().then(() => {
+    if (!isUpdateConstant.value) {
+      return; // Exit if constant updates are not enabled
+    }
+    setTimeout(() => {
+      fetchDataInInterval()
+    }, 100)
+  })
+}
+
+onMounted(() => {
+  fetchDataInInterval();
+});
+
+watch(isUpdateConstant, (newValue) => {
+  if (newValue) {
+    fetchDataInInterval();
+  }
+});
+
+// declare some reactive state here.
+</script>
+
+<template>
+  <h2>Altimeter</h2>
+    <p>Altitude:{{ altitude?.altitude }}</p>
+    <p>TimeStamp:{{ altitude?.timestamp }}</p>
+  <button @click="fetchData" :disabled="isUpdateConstant">Fetch Altimeter Data</button>
+  <!-- checkbox to toggle constant updates -->
+  <label>
+    <input type="checkbox" v-model="isUpdateConstant" />
+    Constant Updates
+  </label>
+  <p v-if="isUpdateConstant">Constant updates are enabled.</p>
+  <p v-else>Constant updates are disabled.</p>
+</template>
