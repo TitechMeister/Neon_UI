3e80994d29e6c508e626e323b9196e80357cd864
Alter <tuuchan0420@gmail.com>
2025-06-08 14:47:58 +0900

    Make toggleswitch of Constantly updating

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref,reactive, onMounted } from 'vue'
+import { ref,watch, onMounted } from 'vue'
 
 interface AltimeterData {
   altitude: number;
@@ -7,6 +7,7 @@ interface AltimeterData {
 }
 
 const altitude = ref<AltimeterData>();
+const isUpdateConstant = ref<boolean>(false);
 
 // component logic
 async function fetchData() {
@@ -24,8 +25,27 @@ async function fetchData() {
   }
 }
 
-fetchData();
-onMounted(fetchData)
+const fetchDataInInterval = () => {
+  fetchData().then(() => {
+    if (!isUpdateConstant.value) {
+      return; // Exit if constant updates are not enabled
+    }
+    setTimeout(() => {
+      fetchDataInInterval()
+    }, 100)
+  })
+}
+
+onMounted(() => {
+  fetchDataInInterval();
+});
+
+watch(isUpdateConstant, (newValue) => {
+  if (newValue) {
+    fetchDataInInterval();
+  }
+});
+
 // declare some reactive state here.
 </script>
 
@@ -36,4 +56,12 @@ onMounted(fetchData)
   <h2>Altimeter</h2>
     <p>Altitude:{{ altitude?.altitude }}</p>
     <p>TimeStamp:{{ altitude?.timestamp }}</p>
+  <button @click="fetchData" :disabled="isUpdateConstant">Fetch Altimeter Data</button>
+  <!-- checkbox to toggle constant updates -->
+  <label>
+    <input type="checkbox" v-model="isUpdateConstant" />
+    Constant Updates
+  </label>
+  <p v-if="isUpdateConstant">Constant updates are enabled.</p>
+  <p v-else>Constant updates are disabled.</p>
 </template>
